cb0938e8bf8e90d5137d129d29349756db6550fb
Mohamed Amine AKIK <223266361+AmineAKIK@users.noreply.github.com>
2026-08-26 11:56:49 +0200

    Fix payment money source contract literals

--- tests/Unit/Domain/PaymentMoneyBoundaryContractTest.php
+++ tests/Unit/Domain/PaymentMoneyBoundaryContractTest.php
@@ -13,10 +13,10 @@ final class PaymentMoneyBoundaryContractTest extends TestCase
         $source = file_get_contents(dirname(__DIR__, 3) . '/src/Services/PaymentLedgerService.php');
         self::assertIsString($source);
 
-        self::assertStringContainsString("$data['montant_cents']", $source);
+        self::assertStringContainsString('$data[\'montant_cents\']', $source);
         self::assertStringContainsString('canonicalCents', $source);
-        self::assertStringNotContainsString("Money::fromDecimal((string) ($data['montant_cents']", $source);
-        self::assertStringNotContainsString("Money::fromDecimal((string) $existing['montant_cents']", $source);
+        self::assertStringNotContainsString('Money::fromDecimal((string) ($data[\'montant_cents\']', $source);
+        self::assertStringNotContainsString('Money::fromDecimal((string) $existing[\'montant_cents\']', $source);
     }
 
     public function testManualPaymentConvertsEuroInputBeforeLedger(): void
@@ -24,8 +24,8 @@ final class PaymentMoneyBoundaryContractTest extends TestCase
         $source = file_get_contents(dirname(__DIR__, 3) . '/src/Controllers/PaiementController.php');
         self::assertIsString($source);
 
-        self::assertStringContainsString("$paymentData['montant_cents'] = Money::fromDecimal", $source);
-        self::assertStringContainsString("unset($paymentData['montant'])", $source);
+        self::assertStringContainsString('$paymentData[\'montant_cents\'] = Money::fromDecimal', $source);
+        self::assertStringContainsString('unset($paymentData[\'montant\'])', $source);
         self::assertStringContainsString("['total_encaisse_cents']", $source);
         self::assertStringContainsString('Money::toDecimalString($encaisseCents)', $source);
     }
@@ -36,7 +36,7 @@ final class PaymentMoneyBoundaryContractTest extends TestCase
         self::assertIsString($source);
 
         self::assertStringContainsString("'montant_cents' => (int) $validated['amount_total']", $source);
-        self::assertStringContainsString("$commandeData['prix_total_cents'] = (int) $validated['amount_total']", $source);
+        self::assertStringContainsString('$commandeData[\'prix_total_cents\'] = (int) $validated[\'amount_total\']', $source);
         self::assertStringNotContainsString('Money::toDecimal(', $source);
     }
 }
